38652d82453fe9ef048b65a569b67ea37b653252
Matt Constenius <mconstenius@gmail.com>
2018-05-13 11:40:52 -0600

    updated

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Matt Constenius on 5/13/18.
 //  Copyright © 2018 Matt Constenius. All rights reserved.
 //
-
+//Motivated
 import UIKit
 
 class ToDoListViewController: UITableViewController {
